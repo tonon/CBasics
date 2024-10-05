96916506900a080d8b69da65733fff81c2ad6c96
tonon <you@example.com>
2024-10-05 14:37:45 -0300

    estruturas de repetiçãõ

--- estruturas/estrutura_selecao.c
+++ estruturas/estrutura_selecao.c
@@ -1,52 +1,55 @@
 #include<stdio.h>
 
+/**
+ * @brief Programa que verifica se um numero é par ou impar
+ * @details O programa calcula o resto da divisão do numero por 2 e verifica se o resultado é igual a zero
+ *          Se sim, o numero é par, caso contrário, é impar
+ */
 int main(void){
 
-int numero, resto;
-float nota1, nota2, media;
-int dia;
+    int numero, resto;
+    float nota1, nota2, media;
+    int dia;
 
 
-//calculo de numero par (divisão por 2 = a zero)
-printf("Digite outro numero inteiro: ");
-scanf("%d", &numero);
-resto = numero%2;
+    //calculo de numero par (divisão por 2 = a zero)
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &numero);
+    resto = numero%2;
 
-
-//estrutura de seleção simples
-if (resto==0){
-    printf ("O numero é par\n");
-}else{
-    printf("O número é impar\n");
-}
-
-
-//Calculo da média de notas de um aluno
-printf("Digite a primeira nota: ");
-scanf("%f", &nota1);
-
-printf("Digite a segunda nota: ");
-scanf("%f", &nota2);
-
-media = (nota1+nota2)/2;
-//estrutura de seleção aninhada
-//Algoritmo que verifica se um aluno foi aprovado, em recuperação ou reprovado
-if(media>=6){
-    printf("Aluno aprovado\n");
-}else{
-    if(media<3){
-        printf("Aluno reprovado\n");
-    }else
-    {
-        printf("Aluno em recuperação\n");
+    //estrutura de seleção simples
+    if (resto==0){
+        printf ("O numero é par\n");
+    }else{
+        printf("O número é impar\n");
     }
-    
-}
 
-//switch/case
-//Nessa estrutura não é possivel testar condicionais nos cases, apenas valores 
+    //Calculo da média de notas de um aluno
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
 
-//Algoritmo que retorna o dia da semana conforme numero informado pelo usuário
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1+nota2)/2;
+
+    //estrutura de seleção aninhada
+    //Algoritmo que verifica se um aluno foi aprovado, em recuperação ou reprovado
+    if(media>=6){
+        printf("Aluno aprovado\n");
+    }else{
+        if(media<3){
+            printf("Aluno reprovado\n");
+        }else
+        {
+            printf("Aluno em recuperação\n");
+        }
+    }
+
+    //switch/case
+    //Nessa estrutura não é possivel testar condicionais nos cases, apenas valores 
+
+    //Algoritmo que retorna o dia da semana conforme numero informado pelo usuário
     printf("Digite um numero de 1 a 7: ");
     scanf("%d", &dia);
     
@@ -78,6 +81,5 @@ if(media>=6){
         break;
     }
 
-
-return 0;
+    return 0;
 }
--- estruturas/estruturas_repeticao.c
+++ estruturas/estruturas_repeticao.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
 
 int main(void){
+int count  = 10;
 
+//laço de repeticão for para imprimir numeros inteiros de 0 a 9
+//Nessa estrutura de repeticão, o valor inicial de i é 0 e o valor final de i é 9, o incremento de i é 1
+//É usado quando o temos o numero de repetições explicitado
+for ( int i = 0; i < count; i++){
+    printf("Numeros de 0 a 9: %d \n", i);
+}
+//laço de repetição while para imprimir numeros inteiros de 0 a 9
+//Nessa estrutura de repeticão, o valor inicial de i é 10 e o valor final de i é 0, o incremento de i é -1
+//É usado quando o temos o numero de repetições indefinido
+while (count > 0){
+    printf("Numeros de 0 a 9: %d \n", count);
+    count--;
+}
+//laço de repeticão do-while para imprimir numeros inteiros de 0 a 9
+//Nessa estrutura de repeticão, o valor inicial de i é 10 e o valor final de i é 0, o incremento de i é -1
+//É usado quando o temos o numero de repetições indefinido e deverá ter pelo menos uma repeticão
+do{
+    printf("Numeros de 0 a 9: %d \n", count);
+    count--;
+}while(count > 0);
 
     return 0;
 }
+
+
+
